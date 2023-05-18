f97479662ea0cb161265f057cab0d92553c9d9bf
Oursel06 <alexoursel6@gmail.com>
2023-05-18 18:20:24 +0200

    supp jwtDecorator

--- src/OpenApi/JwtDecorator.php
+++ src/OpenApi/JwtDecorator.php
@@ -1,34 +1,34 @@
 <?php
 
-namespace App\OpenApi;
+        // namespace App\OpenApi;
 
-use ApiPlatform\Core\OpenApi\Model\OpenApi;
-use ApiPlatform\OpenApi\Factory\OpenApiFactoryInterface;
+        // use ApiPlatform\Core\OpenApi\Model\OpenApi;
+        // use ApiPlatform\OpenApi\Factory\OpenApiFactoryInterface;
 
-final class JwtDecorator implements OpenApiFactoryInterface
-{
-    public function __construct()
-    {
-    }
+        // class JwtDecorator implements OpenApiFactoryInterface
+        // {
+        //     public function __construct()
+        //     {
+        //     }
 
-    public function decorate(OpenApi $openApi): OpenApi
-    {
-        $schemas = $openApi->getComponents()->getSecuritySchemes();
-        $schemas['bearerAuth'] = new \ArrayObject([
-            'type' => 'http',
-            'scheme' => 'bearer',
-            'bearerFormat' => 'JWT',
-        ]);
+        //     public function decorate(OpenApi $openApi): OpenApi
+        //     {
+        //         $schemas = $openApi->getComponents()->getSecuritySchemes();
+        //         $schemas['bearerAuth'] = new \ArrayObject([
+        //             'type' => 'http',
+        //             'scheme' => 'bearer',
+        //             'bearerFormat' => 'JWT',
+        //         ]);
 
-        $openApi->getComponents()->setSecuritySchemes($schemas);
+        //         $openApi->getComponents()->setSecuritySchemes($schemas);
 
-        foreach ($openApi->getPaths()->getPaths() as $pathItem) {
-            $pathItem->getGet()->addSecurity('bearerAuth');
-            $pathItem->getPost()->addSecurity('bearerAuth');
-            $pathItem->getPatch()->addSecurity('bearerAuth');
-            $pathItem->getDelete()->addSecurity('bearerAuth');
-        }
+        //         foreach ($openApi->getPaths()->getPaths() as $pathItem) {
+        //             $pathItem->getGet()->addSecurity('bearerAuth');
+        //             $pathItem->getPost()->addSecurity('bearerAuth');
+        //             $pathItem->getPatch()->addSecurity('bearerAuth');
+        //             $pathItem->getDelete()->addSecurity('bearerAuth');
+        //         }
 
-        return $openApi;
-    }
-}
+        //         return $openApi;
+        //     }
+        // }
